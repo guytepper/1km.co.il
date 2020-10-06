83aa047d1d868982269d79b718e7b5dcf022e292
Omri Dagan <daganomri@gmail.com>
2020-10-06 11:39:07 +0300

    Fixed ProtestList buttons layout issue (#34)

--- src/App.js
+++ src/App.js
@@ -137,18 +137,20 @@ function App() {
                 />
 
                 <ProtestListWrapper>
-                  <Link to="/project-updates/1">
-                    <SiteMessage style={{ backgroundColor: '#6ab04c' }}>
-                      <span style={{ boxShadow: '0 2px 0 0 #fff', fontSize: 19 }}>מה נעשה עכשיו? עדכון פרוייקט #1</span>
-                    </SiteMessage>
-                  </Link>
-                  <Button
-                    color="#3C4F76"
-                    style={{ width: '100%', margin: '0' }}
-                    onClick={() => dispatch({ type: 'setModalState', payload: true })}
-                  >
-                    שינוי כתובת
-                  </Button>
+                  <ProtestListHead>
+                    <Link to="/project-updates/1">
+                      <SiteMessage style={{ backgroundColor: '#6ab04c' }}>
+                        <span style={{ boxShadow: '0 2px 0 0 #fff', fontSize: 19 }}>מה נעשה עכשיו? עדכון פרוייקט #1</span>
+                      </SiteMessage>
+                    </Link>
+                    <Button
+                      color="#3C4F76"
+                      style={{ width: '100%', margin: '0' }}
+                      onClick={() => dispatch({ type: 'setModalState', payload: true })}
+                    >
+                      שינוי כתובת
+                    </Button>
+                  </ProtestListHead>
 
                   <ProtestList closeProtests={state.protests.close} farProtests={state.protests.far} loading={state.loading} />
                   <Footer />
@@ -287,6 +289,10 @@ const ProtestListWrapper = styled.div`
     padding: 0 15px;
     max-height: calc(100vh - 60px);
   }
+`;
+
+const ProtestListHead = styled.div`
+  margin-bottom: 0.5rem;
 `;
 
 export default App;
