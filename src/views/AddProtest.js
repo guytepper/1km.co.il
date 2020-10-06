66b9fe3b64c826dcccb00b202e9237b8ed540ccf
Uri Klar <uriklar@gmail.com>
2020-10-06 14:02:30 +0300

    Add AddProtest view

---
+++ src/views/AddProtest.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ProtestForm } from '../components';
+
+export default function AddProtest({ initialCoords }) {
+  return <ProtestForm initialCoords={initialCoords} />;
+}
