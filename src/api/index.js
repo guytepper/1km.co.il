874812f3fcca188f096a52565d965c221b5f6a69
Guy Tepper <guytepper@gmail.com>
2020-10-06 03:10:53 +0300

    Make the installation process easier (#31)

--- src/api/index.js
+++ src/api/index.js
@@ -26,11 +26,13 @@ export async function createPendingProtest(params) {
   } = params;
 
   try {
+    // Skip protest approval during development
+    const tableName = process.env.NODE_ENV === 'development' ? 'protests' : 'pending_protests';
     // const verification = await verifyRecaptcha(recaptchaToken);
 
     // if (verification.success) {
     const [lat, lng] = coords;
-    const geocollection = GeoFirestore.collection('pending_protests');
+    const geocollection = GeoFirestore.collection(tableName);
 
     const request = geocollection.add({
       displayName,
