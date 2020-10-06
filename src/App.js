b03d841d4a9f6293f15466400eb543f337c322df
Ran Yitzhaki <ranyitz@gmail.com>
2020-10-06 12:04:54 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Map, ProtestList, Footer, Modal, ProtestForm, Button } from './componen
 import { Admin, GroupUpdate, ProjectUpdates } from './views';
 import ProjectSupportPage from './views/ProjectSupportPage';
 import getDistance from 'geolib/es/getDistance';
-import { pointWithinRadius, validateLatLng, isSameCoordinates } from './utils';
+import { pointWithinRadius, validateLatLng } from './utils';
 import styled from 'styled-components';
 import firebase, { firestore } from './firebase';
 import * as geofirestore from 'geofirestore';
