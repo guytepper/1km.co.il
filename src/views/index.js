71882f7c70c6b591d11cebfeafcffe5c14c67b91
Uri Klar <uriklar@gmail.com>
2020-10-08 14:17:30 +0300

    Fix AddProtest view import

--- src/views/index.js
+++ src/views/index.js
@@ -3,3 +3,4 @@ export { default as GroupUpdate } from './GroupUpdate';
 export { default as ProtestPage } from './ProtestPage';
 export { default as ProjectUpdates } from './ProjectUpdates';
 export { default as ProjectSupportPage } from './ProjectSupportPage';
+export { default as AddProtest } from './AddProtest';
