3e0556ab90e9e50a1fceb24747f650f0cc6a29b4
Nir Weber <webernir@Nirs-MacBook-Pro.local>
2020-10-08 23:00:46 +0300

    WIP - Firebase messaging

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/messaging';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,6 +9,7 @@ const config = {
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
 };
 
 firebase.initializeApp(config);
@@ -19,4 +21,28 @@ export const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 export const signOut = () => auth.signOut();
 
+console.log('setting up messaging')
+const messaging = firebase.messaging();
+// messaging.usePublicVapidKey("BLRSg5cI02K8XMW3mKfVOHnKXqRcMk3HBgq8DyQLjfEH-R0Wyi8kGZOvRS-rmyGdGxaYWi7igICLyo0vTlKqsUE");
+messaging.getToken().then((currentToken) => {
+  if (currentToken) {
+    console.log(currentToken)
+    // sendTokenToServer(currentToken);
+    // updateUIForPushEnabled(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    // updateUIForPushPermissionRequired();
+    // setTokenSentToServer(false);
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // showToken('Error retrieving Instance ID token. ', err);
+  // setTokenSentToServer(false);
+});
+
+messaging.onMessage(msg => {
+  console.log({ msg })
+})
 export default firebase;
