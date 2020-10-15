6cd90190bad76bcb45c906d36f9b9ef22c6e6797
guyesh <guy@eshel.net>
2020-10-15 10:41:40 +0300

    removed unneecessary 'aria-labelby

--- src/components/MapSearchAutocomplete/MapSearchAutocomplete.js
+++ src/components/MapSearchAutocomplete/MapSearchAutocomplete.js
@@ -77,7 +77,7 @@ export default function MapSearchAutocomplete({ setStreetAddress, setCoordinates
   };
 
   return (
-    <Combobox style={{ position: 'absolute', zIndex: 10000, width: '100%' }} onSelect={handleSelect} aria-labelledby="demo">
+    <Combobox style={{ position: 'absolute', zIndex: 10000, width: '100%' }} onSelect={handleSelect}>
       <LocationIcon src="/icons/marker-purple.svg" />
       <GpsIcon src="/icons/gps.svg" alt="" onClick={() => getUserPosition()} />
       <ComboboxInputWrapper
