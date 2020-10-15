d00c5ba7f7799d89189a6448856e571f4201ecc9
guyesh <61267328+guyesh@users.noreply.github.com>
2020-10-15 10:36:43 +0300

    remove alt property from icon
    Co-authored-by: Guy Tepper <guytepper@gmail.com>

--- src/components/MapSearchAutocomplete/MapSearchAutocomplete.js
+++ src/components/MapSearchAutocomplete/MapSearchAutocomplete.js
@@ -79,7 +79,7 @@ export default function MapSearchAutocomplete({ setStreetAddress, setCoordinates
   return (
     <Combobox style={{ position: 'absolute', zIndex: 10000, width: '100%' }} onSelect={handleSelect} aria-labelledby="demo">
       <LocationIcon src="/icons/marker-purple.svg" />
-      <GpsIcon src="/icons/gps.svg" alt="gps icon" onClick={() => getUserPosition()} />
+      <GpsIcon src="/icons/gps.svg" alt="" onClick={() => getUserPosition()} />
       <ComboboxInputWrapper
         value={value}
         name="streetAddress"
