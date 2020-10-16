79f497f5790dd6da85f241461e8f585f9c96f275
Guy Tepper <guytepper@gmail.com>
2020-10-16 17:59:29 +0300

    Bloomental

--- src/views/BloomingBalfur.js
+++ src/views/BloomingBalfur.js
@@ -10,13 +10,34 @@ function getRandomCoord({ maxX, maxY }) {
   return [getRandomNumber({ max: maxX }), getRandomNumber({ max: maxY })];
 }
 
+const thronCoordinates = new Array(50)
+  .fill([0, 0])
+  .map(() => ({ bloomed: false, position: getRandomCoord({ maxX: 300, maxY: 480 }) }));
+console.log(thronCoordinates);
+
 export default function BloomingBalfur() {
-  const [flowersCoords, setFlowerCoords] = useState([]);
+  const [thronsList, updateThronsList] = useState(() =>
+    new Array(50).fill([0, 0]).map(() => ({ bloomed: false, position: getRandomCoord({ maxX: 300, maxY: 480 }) }))
+  );
+
   const [flowerCount, setFlowerCount] = useState(1);
 
   useEffect(() => {
-    const coord = getRandomCoord({ maxX: 300, maxY: 480 });
-    setFlowerCoords((prevState) => [...prevState, coord]);
+    // Look for a thron that hasn't bloomed to be a flower
+    let foundThron = false;
+    while (!foundThron) {
+      const index = getRandomNumber({ max: thronsList.length - 1 });
+      if (!thronsList[index].bloomed) {
+        let list = [...thronsList];
+        let item = { ...list[index], bloomed: true };
+        list[index] = item;
+        updateThronsList(list);
+        console.log(item, index, list);
+        foundThron = true;
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [flowerCount]);
 
   return (
@@ -25,10 +46,10 @@ export default function BloomingBalfur() {
         עוד פרח
       </Button>
       <StageWrapper width={360} height={500} options={{ backgroundColor: 0xeef1f5 }}>
-        <Sprite image="/castle.png" width={100} height={100} x={140} y={200} />
-        {flowersCoords.map((coord, index) => {
-          const [x, y] = coord;
-          return <Sprite width={30} height={30} x={x} y={y} image={'/flower.png'} key={index} />;
+        <Sprite image="/castle.png" width={150} height={150} x={220} y={10} />
+        {thronsList.map((thron, index) => {
+          const [x, y] = thron.position;
+          return <Sprite image={thron.bloomed ? '/flower.png' : '/throns.png'} width={20} height={20} x={x} y={y} key={index} />;
         })}
       </StageWrapper>
     </BalfurWrapper>
