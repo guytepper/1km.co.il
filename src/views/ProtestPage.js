5b64d3d8b5b8282c39552a13654e721e400ebd20
Guy Tepper <guytepper@gmail.com>
2020-10-20 10:36:49 +0300

    Loading hebrew text

--- src/views/ProtestPage.js
+++ src/views/ProtestPage.js
@@ -182,7 +182,7 @@ export default function ProtestPage({ user, userCoordinates }) {
 
   if (!protest) {
     // TODO: loading state
-    return <div>Loading...</div>;
+    return <div>טוען...</div>;
   }
 
   const { coordinates, id } = protest;
