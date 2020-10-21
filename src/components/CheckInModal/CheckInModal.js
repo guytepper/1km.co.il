c7ed50f08afe8435adf77272b6ce7f080c240873
Guy Tepper <guytepper@gmail.com>
2020-10-21 08:41:28 +0300

    Added loading step

--- src/components/CheckInModal/CheckInModal.js
+++ src/components/CheckInModal/CheckInModal.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Modal, LocationButtons } from '../';
+import { getLocalStorage } from '../../localStorage';
 
 const steps = {
+  LOADING: 'loading',
   PICK_LOCATION: 'pickLocation',
   PICK_PROTEST: 'pickProtest',
   SIGN_IN: 'signIn',
@@ -9,23 +11,32 @@ const steps = {
   CHECK_IN_FINISHED: 'checkInFinished',
 };
 
-function CheckInModal({ coordinates, setCoordinates, closeProtests }) {
-  const didMountRef = useRef(false);
-  const [currentStep, setCurrentStep] = useState(steps.PICK_LOCATION);
+function CheckInModal({ setCoordinates, closeProtests }) {
+  const [currentStep, setCurrentStep] = useState(steps.LOADING);
 
-  // TODO: Display loadin if app is in loading state
   useEffect(() => {
     if (closeProtests.length > 0) {
+      // If closed protests are available, skip to pick protest step.
       setCurrentStep(steps.PICK_PROTEST);
+    } else if (!getLocalStorage('1km_user_coordinates')) {
+      // In case there are no cached coordinates, change to pick location step.
+      setCurrentStep(steps.PICK_LOCATION);
     }
   }, [closeProtests]);
 
   const renderStep = () => {
     switch (currentStep) {
+      case steps.LOADING:
+        return (
+          <>
+            <p>טוען...</p>
+            <img src="/icons/loading-spinner.svg" alt="" />
+          </>
+        );
       case steps.PICK_LOCATION:
         return <LocationButtons setCoordinates={setCoordinates} />;
       case steps.PICK_PROTEST:
-        return <LocationButtons setCoordinates={setCoordinates} />;
+        return 'pick protest';
 
       default:
         return 'test';
