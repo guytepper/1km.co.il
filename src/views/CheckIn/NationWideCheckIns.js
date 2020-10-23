82a0ef43a290ce8f0982a09da0ab6c634cb92b4f
guyesh <guy@eshel.net>
2020-10-23 11:28:38 +0300

    added NationWIde check ins feed component

---
+++ src/views/CheckIn/NationWideCheckIns.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components/macro';
+
+function NationWideCheckIns({}) {
+  return <></>;
+}
+export default NationWideCheckIns;
