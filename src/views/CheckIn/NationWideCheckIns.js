2f0bfe16882bd58158f08acc2d46193c0158497e
guyesh <guy@eshel.net>
2020-10-23 11:41:05 +0300

    added the css from BalfurCheckIns - as a start point

--- src/views/CheckIn/NationWideCheckIns.js
+++ src/views/CheckIn/NationWideCheckIns.js
@@ -5,3 +5,51 @@ function NationWideCheckIns({}) {
   return <></>;
 }
 export default NationWideCheckIns;
+
+const CheckInsWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px 5px;
+  overflow: hidden;
+`;
+
+const CheckInsList = styled.div`
+  display: grid;
+  grid-auto-rows: 60px;
+  height: 500px;
+`;
+
+const CheckInEntry = styled.div`
+  display: flex;
+  max-width: 320px;
+  align-items: center;
+  margin: 0 12.5px;
+
+  @media (min-width: 600px) {
+    max-width: 400px;
+  }
+`;
+
+const CheckInAvatar = styled.img`
+  width: 32.5px;
+  height: 32.5px;
+  margin-left: 10px;
+  margin-bottom: 5px;
+  flex-shrink: 0;
+  border-radius: 50px;
+  user-select: none;
+`;
+
+const CheckInInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CheckInName = styled.span`
+  font-size: 15px;
+`;
+
+const CheckInComment = styled.span`
+  font-size: 13px;
+  color: #797676;
+`;
