6ab3985cbaef012362c95a789a48f04939da26f9
Guy Tepper <guytepper@gmail.com>
2020-10-28 16:28:50 +0200

    Update PictureFeed.js

--- src/views/LiveEvent/PictureFeed.js
+++ src/views/LiveEvent/PictureFeed.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../../stores';
 import { UploadForm } from '../../components';
-import { savePictureToFirestore } from '../../components/UploadForm/UploadService';
+import { savePictureToFirestore, savePictureToLiveFeed } from '../../components/UploadForm/UploadService';
 
 function PictureFeed() {
-  const afterUpload = async (file) => {
+  const store = useStore();
+
+  // TODO: MOVE OUT
+  store.userStore.setUserProtest('5McvqMWM5jpUXIKdN2Jo');
+
+  const afterUpload = async (file, isAnnonymous) => {
     const imageUrl = file.secure_url;
-    await savePictureToFirestore({ imageUrl, protestId: 'UqcAWCguyNFtgqECTowe', userId: '123' });
+    const { userCurrentProtest, user } = store.userStore;
+    if (!store.userStore.userCurrentProtest) {
+      alert('העלאה נכשלה: התמונה לא משוייכת להפגנה.');
+      return;
+    }
+
+    if (!user?.uid) {
+      alert('העלאה נכשלה: אינך מחובר/ת.');
+      return;
+    }
+
+    const pictureData = { imageUrl, protestId: userCurrentProtest.id };
+
+    if (!isAnnonymous) {
+      pictureData.userId = user.uid;
+    }
+
+    await savePictureToFirestore(pictureData);
+
+    pictureData.protestName = userCurrentProtest.displayName;
+    pictureData.fullName = `${user.firstName || user.first_name} ${user.lastName || user.last_name}`;
+
+    await savePictureToLiveFeed(pictureData);
   };
 
   return <UploadForm afterUpload={afterUpload} />;
 }
 
-export default PictureFeed;
+export default observer(PictureFeed);
