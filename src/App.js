b5c3e3ba57e8c5193028ef2ead90f5987dd4dcf5
Guy Tepper <guytepper@gmail.com>
2020-10-30 17:12:56 +0200

    Hide live header icon
    Instead of removing it from the DOM, which makes the logo align to the right

--- src/App.js
+++ src/App.js
@@ -79,9 +79,9 @@ function App() {
       <AppWrapper>
         <Router>
           <Header>
-            {/* <NavItemLive to="/live">
+            <NavItemLive to="/live">
               <LiveIcon src="/icons/live.svg" alt="" style={{ marginRight: 10 }} />
-            </NavItemLive> */}
+            </NavItemLive>
             <Link to="/">
               <img src="/logo.svg" alt=" קילומטר אחד" />
             </Link>
@@ -201,8 +201,7 @@ const Header = styled.header`
   top: 0;
   justify-content: space-between;
   align-items: center;
-  /* padding: 5px 8px 5px 20px; */ // Uncomment when going live
-  padding: 5px 20px 5px;
+  padding: 5px 8px 5px 20px;
   grid-row: 1;
   background-color: #fff;
   box-shadow: #e1e4e8 0px -1px 0px inset, #00000026 0px 4px 5px -1px;
@@ -227,6 +226,7 @@ const NavItemLive = styled(Link)`
   font-weight: bold;
   font-size: 18px;
   animation: ${fadeIn} 1.2s linear 1s infinite alternate;
+  visibility: hidden; // Remove when going live */
 `;
 
 const NavProfileWrapper = styled.div`
--- src/views/LiveEvent/event_data.js
+++ src/views/LiveEvent/event_data.js
@@ -1,1 +1,1 @@
-export const EVENT_DATE = '31-10-20_BETA';
+export const EVENT_DATE = '31-10-20_TEST';
