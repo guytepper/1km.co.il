d5e5dd6acb5b9c94daa5db5bc261e2ed01042c65
Guy Tepper <guytepper@gmail.com>
2020-11-02 00:08:55 +0200

    Additions

--- src/views/ProtestPictures/ProtestPictures.js
+++ src/views/ProtestPictures/ProtestPictures.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import { ProtestPicturesWrapper, HeroImage, ProtestPictureSection, InfoBox, ProtestImage } from './ProtestPicturesElements';
+import {
+  ProtestPicturesWrapper,
+  ProtestPicturesHeader,
+  ProtestPicturesSubheader,
+  ProtestPicturesText,
+  HeroImage,
+  ProtestPictureSection,
+  InfoBox,
+  ProtestImage,
+  ImageLocation,
+  ImageCredit,
+} from './ProtestPicturesElements';
+import './ProtestPictures.css';
 
 function ProtestPictures() {
   return (
@@ -11,7 +23,7 @@ function ProtestPictures() {
         </HeroImage.TextWrapper>
       </HeroImage>
       <ProtestPictureSection imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604253080/weekend_pictures/31-10-2020/first-bg_uozjrt.jpg">
-        <InfoBox>
+        <InfoBox id="info-box">
           <p>בסוף השבוע האחרון התחלנו לאפשר העלאת תמונות לאתר קילומטר אחד.</p>
           <p>
             עשרות תמונות נשלחו, ורצינו ליצור מקום שייתן במה לתמונות נבחרות מדי שבוע - בנוסף לעמודי ההפגנות באתר שם אפשר לצפות
@@ -21,22 +33,19 @@ function ProtestPictures() {
           <p>אפשר להעלות תמונות במהלך השבוע, ובעיקר בכל יום שבת.</p>
           <p>עקבו אחרינו בפייסבוק לקבלת עדכונים!</p>
         </InfoBox>
-        <ProtestImage imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604256862/weekend_pictures/31-10-2020/pinkfront.jpg" />
-        <ProtestImage imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604256316/weekend_pictures/31-10-2020/gesher01.jpg" />
-        <ProtestImage imageUrl="https://res.cloudinary.com/onekm/image/upload/c_fill,g_auto,h_700,w_570/v1604257020/weekend_pictures/31-10-2020/submaringate-wrong-person-arrested.jpg">
-          <ProtestImage.Description>
-            <p>
-              רועי פלג, ממובילי תנועת חקירה עכשיו, נעצר במהלך צעדת מחאה בכביש 1.{' '}
-              <a
-                href="https://www.facebook.com/roey.peleg/posts/10224908923856122?__cft__[0]=AZXuTo6QNqEa0PlpxdU_kg2AGJ7JmDkq9hekO7zFCR-bNDCsapt4zQO3bJA1sbCxFr8pu4rh8w9GiSqKhL6dIqAsYumuIWtkQ6rV64HHcMSmgdZ8-Wga0G8ipalzn3pt5os&__tn__=%2CO%2CP-R"
-                target="_blank"
-              >
-                לפוסט בפייסבוק
-              </a>
-            </p>
-          </ProtestImage.Description>
-        </ProtestImage>
-        <ProtestImage imageUrl="https://res.cloudinary.com/onekm/image/upload/c_fill,g_auto,h_700,w_570/v1604257020/weekend_pictures/31-10-2020/submaringate-wrong-person-arrested.jpg">
+        <ProtestImage
+          id="gesher"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604256316/weekend_pictures/31-10-2020/gesher01.jpg"
+        />
+        <ProtestImage
+          id="pinkfront"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604256862/weekend_pictures/31-10-2020/pinkfront.jpg"
+        />
+        <ProtestImage imageUrl="https://res.cloudinary.com/onekm/image/upload/c_scale,q_auto:low,w_740/v1604261694/weekend_pictures/31-10-2020/IMG_1559_qfhq7k.jpg"></ProtestImage>
+        <ProtestImage
+          id="roey-peleg-arrest"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/c_fill,g_auto,h_700,w_570/v1604257020/weekend_pictures/31-10-2020/submaringate-wrong-person-arrested.jpg"
+        >
           <ProtestImage.Description>
             <p>
               רועי פלג, ממובילי תנועת חקירה עכשיו, נעצר במהלך צעדת מחאה בכביש 1.{' '}
@@ -51,6 +60,54 @@ function ProtestPictures() {
           </ProtestImage.Description>
         </ProtestImage>
       </ProtestPictureSection>
+      <ProtestPictureSection
+        style={{ backgroundPosition: 'top' }}
+        imageUrl="https://res.cloudinary.com/onekm/image/upload/q_auto:eco/v1604264028/weekend_pictures/31-10-2020/rabin-sqaure_pmcyeu.jpg"
+      >
+        <div>
+          <ProtestPicturesHeader>זוכרים את הרצח</ProtestPicturesHeader>
+          <ProtestPicturesSubheader style={{ marginBottom: 10 }}>כיכר רבין, 29/30/20</ProtestPicturesSubheader>
+          <ProtestPicturesText>ברחבי הארץ צוין יום הזכרון לרצח ראש הממשלה יצחק רבין.</ProtestPicturesText>
+          <ProtestPicturesText>
+            בימים אלו, כשראש ממשלה מכהן מסית כנגד חלק ניכר מהעם, חשוב לזכור את נקודת הקצה שמדינת ישראל הגיעה אליה לפני 25 שנה
+            ולהתנגד בכל הכח להסתה.
+          </ProtestPicturesText>
+        </div>
+        <p style={{ gridRow: 2, fontSize: 14, color: '#fff8f8db' }}>צילום: רונן טופלברג</p>
+      </ProtestPictureSection>
+      <ProtestPictureSection style={{ background: '#000' }} id="rabin-square-picures">
+        <ProtestImage
+          id="candles"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/q_auto:low/v1604264691/weekend_pictures/31-10-2020/candles_gf8czy.jpg"
+        >
+          <ImageCredit>צילום: אורן זיו</ImageCredit>
+        </ProtestImage>
+        <ProtestImage
+          id="candles2"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604264691/weekend_pictures/31-10-2020/candles2_kxjbzp.jpg"
+        >
+          <ImageCredit>צילום: אורן זיו</ImageCredit>
+        </ProtestImage>
+        <ProtestImage
+          id="clock-square-rabin"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/q_auto:low/v1604266088/weekend_pictures/31-10-2020/clock-square-rabin_u5wwtg.jpg"
+        >
+          <ImageLocation>כיכר השעון, יפו</ImageLocation>
+        </ProtestImage>
+        <ProtestImage
+          id="zion-square-rabin"
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/v1604266150/weekend_pictures/31-10-2020/zion_square_egjtwi.jpg"
+        >
+          <ImageLocation>כיכר ציון, ירושלים</ImageLocation>
+        </ProtestImage>
+        <ProtestImage
+          id=""
+          imageUrl="https://res.cloudinary.com/onekm/image/upload/q_auto:low/v1604266437/weekend_pictures/31-10-2020/givaat_amal_rabin_ykvd6p.jpg"
+        >
+          <ImageLocation>טקס זכרון, גבעת עמל</ImageLocation>
+        </ProtestImage>
+      </ProtestPictureSection>
+      <ProtestPictureSection style={{ background: '#d83039' }}></ProtestPictureSection>
     </ProtestPicturesWrapper>
   );
 }
--- src/views/ProtestPictures/ProtestPicturesElements.js
+++ src/views/ProtestPictures/ProtestPicturesElements.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-export const ProtestPicturesWrapper = styled.div``;
+export const ProtestPicturesWrapper = styled.div`
+  h2,
+  h3 {
+    font-family: Caravan, sans-serif;
+    color: white;
+    text-shadow: 0 1px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
+
+export const ProtestPicturesHeader = styled.h2`
+  margin: 0;
+  font-size: 38px;
+`;
+
+export const ProtestPicturesSubheader = styled.h2`
+  font-size: 22px;
+  font-family: Simpler, sans-serif !important;
+`;
+
+export const ProtestPicturesText = styled.p`
+  max-width: 88vw;
+  margin-bottom: 8px;
+  font-size: 17px;
+  font-family: Simpler, sans-serif !important;
+  color: #fff;
+`;
 
 export const HeroImage = styled.div`
   display: grid;
@@ -13,13 +38,6 @@ export const HeroImage = styled.div`
   background-repeat: no-repeat;
   background-position: 52%;
   background-size: cover;
-
-  h2,
-  h3 {
-    font-family: Caravan, sans-serif;
-    color: white;
-    text-shadow: 0 1px 10px rgba(0, 0, 0, 0.5);
-  }
 `;
 
 HeroImage.TextWrapper = styled.div`
@@ -47,13 +65,20 @@ HeroImage.Subtitle = styled.h3`
 export const ProtestPictureSection = styled.div`
   width: 100vw;
   padding: 25px;
-  min-height: 90vh;
+  min-height: 95vh;
   background-image: ${({ imageUrl }) => `url('${imageUrl}')`};
 
   display: grid;
   grid-template-columns: 1fr;
-  grid-auto-rows: max-content;
+  grid-auto-rows: minmax(420px, max-content);
   gap: 20px;
+
+  @media (min-width: 1024px) {
+    padding: 5% 10%;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 235px max-content;
+    grid-auto-rows: unset;
+  }
 `;
 
 export const InfoBox = styled.div`
@@ -62,26 +87,48 @@ export const InfoBox = styled.div`
   background: #fff;
   text-align: center;
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+
+  @media (min-width: 1024px) {
+    padding: 30px;
+  }
 `;
 
-export const ProtestImage = ({ children, imageUrl }) => {
+export const ProtestImage = ({ children, imageUrl, id }) => {
   return (
-    <div style={{ boxShadow: '0 0 3px 0 rgba(0, 0, 0, 0.5);' }}>
-      <img src={imageUrl} alt="" style={{ width: '100%' }} />
+    <ProtestImageWrapper id={id}>
+      <img src={imageUrl} alt="" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       {children}
-    </div>
+    </ProtestImageWrapper>
   );
 };
 
+const ProtestImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+`;
+
 ProtestImage.Description = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: start;
   min-height: 60px;
   padding: 12.5px;
   background: rgba(0, 0, 0, 0.6);
   color: #fff;
+`;
 
-  p {
-    margin: 0;
-  }
+export const ImageLocation = styled.p`
+  margin: -30px 10px;
+  text-align: right;
+  color: white;
+  text-shadow: 0 0 2px #000;
+`;
+
+export const ImageCredit = styled.p`
+  margin: -30px 10px;
+  font-size: 14px;
+  color: white;
+  text-align: left;
+  text-shadow: 0 0 2px #000;
 `;
