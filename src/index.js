508634665f5d9a9a89266f99c54e3ea17a8b1dcd
Guy Tepper <guytepper@gmail.com>
2020-11-03 00:38:21 +0200

    Added react-snap

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import './index.css';
 import { StoreProvider } from './stores';
 import App from './App';
 
-ReactDOM.render(
+const ReactApp = () => (
   <React.StrictMode>
     <StoreProvider>
       <ConfigProvider locale={heIL} direction="rtl">
         <App />
       </ConfigProvider>
     </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<ReactApp />, rootElement);
+} else {
+  ReactDOM.render(<ReactApp />, rootElement);
+}
 
 /* Detect android devies for device-specificissues
 const html = document.querySelector('body');
