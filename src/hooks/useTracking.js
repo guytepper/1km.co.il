f7a4aea06be4ecb2b70104e0e27649a796502014
Guy Tepper <guytepper@gmail.com>
2020-11-06 20:12:49 +0200

    Uncomment useTracking code

--- src/hooks/useTracking.js
+++ src/hooks/useTracking.js
@@ -3,9 +3,10 @@ import { useLocation } from 'react-router-dom';
 import { analytics } from '../firebase';
 
 export const useTracking = () => {
-  // let location = useLocation();
-  // useEffect(() => {
-  //   const page_path = location.pathname;
-  //   analytics().logEvent('page_view', { page_path });
-  // }, [location]);
+  let location = useLocation();
+
+  useEffect(() => {
+    const page_path = location.pathname;
+    analytics.logEvent('page_view', { page_path });
+  }, [location]);
 };
