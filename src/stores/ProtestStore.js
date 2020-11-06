2f467988b613e6fefc1b6ab52f13a7e390705619
Guy Tepper <guytepper@gmail.com>
2020-11-06 11:48:51 +0200

    Comment out protest shifting code

--- src/stores/ProtestStore.js
+++ src/stores/ProtestStore.js
@@ -14,15 +14,18 @@ class ProtestStore {
   get closeProtests() {
     if (this.nearbyProtests.length > 0) {
       const closeList = this.nearbyProtests.filter((p) => p.distance < 2000).sort((p1, p2) => p1.distance - p2.distance);
-
-      // Temporary code for Ramart Gan - Givayayim march.
-      const marchProtest = this.nearbyProtests.find((p) => p.id === 'eoLv9Kb4x3sFBcAkcdMh');
-      console.log(marchProtest.id);
-      if (marchProtest) {
-        closeList.splice(closeList.indexOf(marchProtest), 1);
-        closeList.unshift(marchProtest);
-      }
       return closeList;
+
+      // Use in times when need to push a protest up in the list
+      // For example, during marches, we want the protest to be the first available even if
+      // the protest location is not the closest to the user.
+
+      // const marchProtest = this.nearbyProtests.find((p) => p.id === 'eoLv9Kb4x3sFBcAkcdMh');
+      // if (marchProtest) {
+      //   closeList.splice(closeList.indexOf(marchProtest), 1);
+      //   closeList.unshift(marchProtest);
+      // }
+      // return closeList;
     }
 
     return [];
