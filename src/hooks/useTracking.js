d8a07114af3e4efdef633f113fb57c7406486f94
Guy Tepper <guytepper@gmail.com>
2020-11-06 20:17:43 +0200

    Added hook description

--- src/hooks/useTracking.js
+++ src/hooks/useTracking.js
@@ -2,6 +2,10 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { analytics } from '../firebase';
 
+/**
+ * Tracks page views through firebase analytics.
+ */
+
 export const useTracking = () => {
   let location = useLocation();
 
