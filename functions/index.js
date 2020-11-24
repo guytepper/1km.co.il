f8bf3c0ae097ade3b2ad7e0556db9a57d826626d
Omri08 <omrimalka08@gmail.com>
2020-11-24 17:12:32 +0200

    Added environment  variables to the cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,25 +2,26 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require('path');
 const os = require('os');
-const env = require('dotenv').config();
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const { nanoid } = require('nanoid');
 
-const { Storage } = require('@google-cloud/storage');
-const gcs = new Storage();
-admin.initializeApp();
+const { databaseURL, storageBucket } = JSON.parse(process.env.FIREBASE_CONFIG);
+admin.initializeApp({
+  projectId: process.env.GCLOUD_PROJECT,
+  storageBucket: storageBucket,
+  databaseURL: databaseURL,
+});
 
 exports.onUserCreate = functions.firestore.document('/users/{userId}').onCreate(async (snap, context) => {
   // Grab the current value of what was written to Cloud Firestore.
   const pictureUrl = snap.data().pictureUrl;
-  const bucket = gcs.bucket(process.env.FIREBASE_STORAGE);
-  const filename = `${nanoid()}.jpeg`;
-  const filePath = `profile_pics/${filename}`;
+  const bucket = admin.storage().bucket();
+  const filename = 'profile.jpeg';
+  const filePath = `users/${context.params.userId}/${filename}`;
   const tempFilePath = path.join(os.tmpdir(), filename);
 
   try {
-   // Download the image
+    // Download the image
     const img = await fetch(pictureUrl);
     const imgBlob = img.body;
     const fileStream = fs.createWriteStream(tempFilePath);
@@ -31,16 +32,18 @@ exports.onUserCreate = functions.firestore.document('/users/{userId}').onCreate(
         reject(pictureUrl);
       });
 
-      // Upload the image to our storage 
+      // Upload the image to our storage
       fileStream.on('finish', async () => {
         const file = await bucket.upload(tempFilePath, {
           destination: filePath,
           predefinedAcl: 'publicRead',
+          public: 'true',
+          contentType: 'image/jpeg',
         });
         resolve(file[0].metadata.mediaLink);
       });
     });
-    functions.logger.log('Changed user picutre url', context.params.userId);
+    functions.logger.log('Changed user picutre url', context.params.userId, newPictureUrl);
     return snap.ref.set({ pictureUrl: newPictureUrl }, { merge: true });
   } catch (error) {
     return new functions.https.HttpsError('cancelled', error);
